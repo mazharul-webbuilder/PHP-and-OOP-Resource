6d13e8476fbf251bc5dbfdeaa55a5366b9686ed0
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-13 22:18:18 +0600

    abstract class

---
+++ abstract_class/Database.php
@@ -0,0 +1,42 @@
+<?php
+
+abstract class Database
+{
+    public string $connection;
+    public bool $connectionActive;
+    public function __construct($connection, $connectionActive)
+    {
+        $this->connection = $connection;
+        $this->connectionActive = $connectionActive;
+    }
+
+    public abstract function setConnection(bool $connection_status);
+    public abstract function getConnection();
+}
+
+class DBconnection extends Database
+{
+    public function __construct($connection, $connectionActive)
+    {
+        parent::__construct($connection, $connectionActive);
+    }
+
+    public function getConnection(): void
+    {
+        if ($this->connectionActive) {
+            echo "Connection Active" . PHP_EOL;
+        } else {
+            echo "Disconnected" . PHP_EOL;
+        }
+    }
+
+    public function setConnection(bool $connection_status)
+    {
+        $this->connectionActive = $connection_status;
+
+        return $this; // return form method chaining
+    }
+}
+
+$dbConnection = new DBconnection("What is the Status", true);
+$dbConnection->setConnection(false)->getConnection(); // Here method changing Used
