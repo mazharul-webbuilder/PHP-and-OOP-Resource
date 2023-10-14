aba9c092d24bd3403918aab87970478b1c3577fa
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-14 12:00:08 +0600

    encapsulation

---
+++ encapsulation/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+class Student
+{
+    private $id;
+    private $name;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function getId()
+
+    {
+        return $this->id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+
+}
