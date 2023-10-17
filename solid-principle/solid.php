398c2c8cc0bc8278eca56b2c60cf6914c1e82df2
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-17 21:01:25 +0600

    solid principle

---
+++ solid-principle/solid.php
@@ -0,0 +1,7 @@
+<?php
+//SOLID
+//S = Single Responsibility Principle
+//O = Open Closed Principle
+//L = Liskov Substitution Principle
+//I = Interface Segregation Principle
+//D = Dependency Inversion Principle
