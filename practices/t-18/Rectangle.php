c0b6fb4fcdbe9bfba63c25e6d6ae33dba24f1990
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-18 20:54:32 +0600

    t-18

---
+++ practices/t-18/Rectangle.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+class Rectangle
+{
+    private int|float $length;
+    private int|float $width;
+
+    public function __construct($length, $width)
+    {
+        $this->length = $length;
+        $this->width = $width;
+    }
+
+    public function getArea(): int|float
+    {
+        return $this->length * $this->width;
+    }
+
+    public function getPerimeter(): int|float
+    {
+        return 2*($this->length + $this->width);
+    }
+
+}
+
+$myfirstReactangle = new Rectangle(10, 10.20);
+echo $myfirstReactangle->getArea() . PHP_EOL;
+echo $myfirstReactangle->getPerimeter() . PHP_EOL;
