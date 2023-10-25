d12e77a8693dad92de1484e5b5dd97bcfb4b9c5c
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-25 21:33:22 +0600

    25 oct 2023

--- practices/t-18/p-19/Validation.php
+++ practices/t-18/p-19/Validation.php
@@ -1,16 +1,40 @@
 <?php
-
-class Validation
-{
-    protected array $request;
-
-    public function __construct($request)
-    {
-        $this->request = $request;
+class Validation {
+    public static function validateEmail($email) {
+        // Check if the email is valid using a regular expression
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
 
-    public function validate(){
-
+    public static function validatePassword($password) {
+        // Here's an example that checks if the password is at least 8 characters long
+        return strlen($password) >= 8;
     }
 
+    public static function validateField($field) {
+        // Here's an example that checks if the field is not empty
+        return !empty($field);
+    }
 }
+
+$email = "admin@example.com";
+$password = "password123";
+$field = "";
+
+if (Validation::validateEmail($email)) {
+    echo "Email is valid.</br>";
+} else {
+    echo "Email is invalid.</br>";
+}
+
+if (Validation::validatePassword($password)) {
+    echo "Password is valid.</br>";
+} else {
+    echo "Password is invalid.v";
+}
+
+if (Validation::validateField($field)) {
+    echo "Field is valid.</br>";
+} else {
+    echo "Field is invalid.</br>";
+}
+?>
