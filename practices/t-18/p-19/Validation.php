15e383e2d5befaec53b3f3de498fd2f9a42ad8a0
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-25 21:32:43 +0600

    25 oct 2023

---
+++ practices/t-18/p-19/Validation.php
@@ -0,0 +1,16 @@
+<?php
+
+class Validation
+{
+    protected array $request;
+
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+
+    public function validate(){
+
+    }
+
+}
