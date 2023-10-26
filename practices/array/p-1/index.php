ff953eaddb2db54bc0a206ace5323017d34b7683
IROZEN-DEV3 <mazharul.irozen@gmail.com>
2023-10-26 15:06:08 +0600

    26 october 2023

---
+++ practices/array/p-1/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$colors = ['white', 'green', 'red', 'blue', 'black'];
+
+echo "The memory of that scene for me is like a frame of film forever frozen at 
+that moment: the $colors[2] carpet, the $colors[1] lawn, the white house, the leaden sky. 
+The new president and his first lady. - Richard M. Nixon";
