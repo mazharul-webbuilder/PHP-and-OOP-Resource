8a796b9e7b63c56fd7eefd65d287f85f07ae8fc8
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-27 16:29:13 +0600

    array 9

---
+++ practices/array/learn/array_cheat_sheet.php
@@ -0,0 +1,29 @@
+<?php
+
+//Returns a new array, appending input arrays with array unpacking
+//$arr = [1, 2, 3, 4, 5];
+//$output = [...$arr, ...[10, 11, 12]];
+//print_r($output);
+
+//Returns a new array, appending input arrays with array_merge
+//$arr = [1, 2, 4, 5];
+//$arr2 = [23,45,54];
+//$merged_array = array_merge($arr, $arr2);
+//print_r($merged_array);
+
+//Joins all elements into a string with a separator
+//$arr = [1, 2, 4, 5];
+//$make_array_as_string = implode(',' ,$arr);
+//echo $make_array_as_string;
+
+//Returns a section of an array from $start to $end
+//$start = 0;
+//$end = 3;
+//$arr = [1, 2, 3, 4, 5, 6];
+//$sliced_array = array_slice($arr, $start, $end);
+//print_r($sliced_array);
+
+//Remove a element from an array and replace with another value
+//$arr = [1, 2, 3, 4, 5, 6];
+//array_splice($arr, 2, 1, 33 );
+//print_r($arr);
