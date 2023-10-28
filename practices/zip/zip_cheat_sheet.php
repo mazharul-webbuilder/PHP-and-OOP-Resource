7f32e1f8b59a4c698d7955ac2cd0cbfcfcac5560
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-28 22:14:13 +0600

    function

---
+++ practices/zip/zip_cheat_sheet.php
@@ -0,0 +1,10 @@
+zip_close()	Closes a ZIP file archive
+zip_entry_close()	Closes a ZIP directory entry
+zip_entry_compressedsize()	Returns the compressed file size of a ZIP directory entry
+zip_entry_compressionmethod()	Returns the compression method of a ZIP directory entry
+zip_entry_filesize()	Returns the actual file size of a ZIP directory entry
+zip_entry_name()	Returns the name of a ZIP directory entry
+zip_entry_open()	Opens a directory entry in a ZIP file for reading
+zip_entry_read()	Reads from an open directory entry in the ZIP file
+zip_open()	Opens a ZIP file archive
+zip_read()	Reads the next file in a open ZIP file archive
