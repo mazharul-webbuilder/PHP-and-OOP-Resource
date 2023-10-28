c3f5ff3f57a1aa5774505619b0e85c8f5006c6a3
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-28 22:03:08 +0600

    function

---
+++ practices/directory/directory_cheat_sheet.php
@@ -0,0 +1,12 @@
+Here's a list of directory management functions in PHP, including the functions you mentioned:
+
+chdir(): Changes the current directory.
+chroot(): Changes the root directory.
+closedir(): Closes a directory handle.
+dir(): Returns an instance of the Directory class (not recommended for new code, as it's deprecated since PHP 7.0).
+getcwd(): Returns the current working directory.
+opendir(): Opens a directory handle.
+readdir(): Returns an entry from a directory handle.
+rewinddir(): Resets a directory handle.
+scandir(): Returns an array of files and directories in a specified directory.
+These functions are used for various directory-related operations in PHP, such as changing directories, reading directory contents, and managing directory handles.
