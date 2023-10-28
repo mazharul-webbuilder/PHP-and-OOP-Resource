b0109f986fc7c6af9d7b03e4c9d63aa7f2dc2274
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-28 14:30:07 +0600

    is_array() function

---
+++ practices/array/learn/is_array()_funcation.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//Checks if a variable is an array.
+
+$fruits = array('Banana', 'Mango', 'Apple');
+
+if (is_array($fruits)) {
+    echo "This is a array";
+} else {
+    echo "Not an array";
+}
