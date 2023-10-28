9c178d2a7c26190889ffe7926a5bec9db448c416
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-10-28 20:46:30 +0600

    function

---
+++ practices/math/math_cheat_sheet.php
@@ -0,0 +1,45 @@
+PHP provides a variety of mathematical functions that you can use for performing mathematical operations. Here's a list of some commonly used mathematical functions in PHP:
+
+Basic Arithmetic Functions:
+
+abs(): Returns the absolute value of a number.
+sqrt(): Returns the square root of a number.
+pow(): Raises a number to a specified power.
+exp(): Returns the exponential value of a number.
+log(): Returns the natural logarithm of a number.
+log10(): Returns the base-10 logarithm of a number.
+round(): Rounds a floating-point number to the nearest integer.
+floor(): Rounds a number down to the nearest integer.
+ceil(): Rounds a number up to the nearest integer.
+rand(): Generates a random integer.
+Trigonometric Functions:
+
+sin(): Returns the sine of an angle.
+cos(): Returns the cosine of an angle.
+tan(): Returns the tangent of an angle.
+asin(): Returns the arcsine of a number.
+acos(): Returns the arccosine of a number.
+atan(): Returns the arctangent of a number.
+atan2(): Calculates the arctangent of two variables.
+Advanced Mathematical Functions:
+
+deg2rad(): Converts degrees to radians.
+rad2deg(): Converts radians to degrees.
+Exponential and Logarithmic Functions:
+
+exp(): Returns the exponential value of a number.
+log(): Returns the natural logarithm of a number.
+log10(): Returns the base-10 logarithm of a number.
+Number Comparison Functions:
+
+max(): Returns the highest value in an array or list of arguments.
+min(): Returns the lowest value in an array or list of arguments.
+abs(): Returns the absolute value of a number.
+Random Number Functions:
+
+rand(): Generates a random integer.
+mt_rand(): Generates a random integer using the Mersenne Twister algorithm.
+Number Formatting Functions:
+
+number_format(): Formats a number with thousands separators.
+These are some of the commonly used mathematical functions in PHP. PHP offers many more math-related functions to suit various needs. You can refer to the PHP documentation for a comprehensive list and detailed descriptions of mathematical functions in PHP.
