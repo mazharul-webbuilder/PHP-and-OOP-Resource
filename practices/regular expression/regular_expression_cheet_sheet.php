a8b05f1a8bf49af9b6a6db841229b704b95db272
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-11-01 18:30:26 +0600

    regular expresion

--- practices/regular expression/regular_expression_cheet_sheet.php
+++ practices/regular expression/regular_expression_cheet_sheet.php
@@ -1,16 +1,28 @@
-PHP provides various functions for working with regular expressions. Here's a list of some commonly used regular expression functions in PHP:
+<?php
+/**
+ * PHP provides various functions for working with regular expressions.
+ * Here's a list of some commonly used regular expression functions in PHP:
+*/
+/*-------------------------------------------------------------------*/
 
-preg_match($pattern, $subject, &$matches, $flags = 0, $offset = 0): Performs a regular expression match and stores the matched groups in an array.
-preg_match_all($pattern, $subject, &$matches, $flags = PREG_PATTERN_ORDER, $offset = 0): Performs a global regular expression match and stores all matches in a 2D array.
-preg_replace($pattern, $replacement, $subject, $limit = -1, &$count = null): Performs a regular expression search and replace and returns the modified string.
-preg_replace_callback($pattern, $callback, $subject, $limit = -1, &$count = null): Performs a regular expression search and replace using a callback function.
-preg_filter($pattern, $replacement, $subject, $limit = -1, &$count = null): Performs a regular expression search and replace and returns the modified string without affecting the original subject.
-preg_grep($pattern, $input, $flags = 0): Returns an array of elements that match the given regular expression.
-preg_split($pattern, $subject, $limit = -1, $flags = 0): Splits a string by a regular expression.
-preg_quote($str, $delimiter = null): Quote regular expression characters.
-preg_last_error(): Returns the error code of the last PCRE regex execution.
-These functions are used for pattern matching and manipulation of strings using regular expressions in PHP. Be sure to consult the official PHP documentation for the most up-to-date information and examples of how to use these functions effectively.
+//Performs a regular expression match and stores the matched groups in an array.
 
+$pattern = "/w3schools/i";
+$subject = "Visit W3Schools";
 
+echo preg_match($pattern, $subject) . PHP_EOL;
 
-
+/*-------------------------------------------------------------------*/
+//preg_match_all($pattern, $subject, &$matches, $flags = PREG_PATTERN_ORDER, $offset = 0): Performs a global regular expression match and stores all matches in a 2D array.
+//preg_replace($pattern, $replacement, $subject, $limit = -1, &$count = null): Performs a regular expression search and replace and returns the modified string.
+//preg_replace_callback($pattern, $callback, $subject, $limit = -1, &$count = null): Performs a regular expression search and replace using a callback function.
+//preg_filter($pattern, $replacement, $subject, $limit = -1, &$count = null): Performs a regular expression search and replace and returns the modified string without affecting the original subject.
+//preg_grep($pattern, $input, $flags = 0): Returns an array of elements that match the given regular expression.
+//preg_split($pattern, $subject, $limit = -1, $flags = 0): Splits a string by a regular expression.
+//preg_quote($str, $delimiter = null): Quote regular expression characters.
+//preg_last_error(): Returns the error code of the last PCRE regex execution.
+//These functions are used for pattern matching and manipulation of strings using regular expressions in PHP. Be sure to consult the official PHP documentation for the most up-to-date information and examples of how to use these functions effectively.
+//
+//
+//
+//
