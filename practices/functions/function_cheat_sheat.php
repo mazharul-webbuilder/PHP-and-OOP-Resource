439ca898211ebb541a653786e127e91be57e5c0d
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-11-02 21:05:26 +0600

    built in function in php

---
+++ practices/functions/function_cheat_sheat.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHP has a good deal of build in functions
+ */
+
+/*--------------------------------------------------------------------------------------*/
+//All build in function available in php 8.2 found in below official link
+//https://www.php.net/manual/en/indexes.functions.php
+/*--------------------------------------------------------------------------------------*/
+
+//Display value with data type
+var_dump(2); // output: int(2)
+
+
+/*--------------------------------------------------------------------------------------*/
+
