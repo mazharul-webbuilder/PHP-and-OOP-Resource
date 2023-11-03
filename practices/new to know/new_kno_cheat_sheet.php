dfe06b246f884afc71f363361d450026fa8efc68
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-11-03 14:51:21 +0600

    php magic constatnt

--- practices/new to know/new_kno_cheat_sheet.php
+++ practices/new to know/new_kno_cheat_sheet.php
@@ -17,3 +17,26 @@ $messasge = match ($paymentStatus){
 };
 
 var_dump($messasge); // output: success
+//----------------------------------------------------//
+
+/*Predefined PHP_Contant Variable*/
+//like PHP_VERSION, PHP_EOL, PHP_INT_MAX, PHP_INT_MAX ........................
+
+
+//----------------------------------------------------//
+
+//Magic Constants, PHP offer  9 magic constants
+
+/*__LINE__  this will the return the current line number*/
+
+echo __LINE__ . PHP_EOL; // Output: 25
+echo __FILE__ . PHP_EOL; // Output: C:\xampp\htdocs\oop\practices\new to know\new_kno_cheat_sheet.php
+echo __DIR__ . PHP_EOL; // Output: C:\xampp\htdocs\oop\practices\new to know
+echo __FUNCTION__ . PHP_EOL; // It returns the name of the current function
+echo __CLASS__ . PHP_EOL; // It returns the name of the current class
+echo __TRAIT__ . PHP_EOL; // It returns the name of the current trait
+echo __METHOD__ . PHP_EOL; // It returns the name of the current method (function in a class).
+echo __NAMESPACE__ . PHP_EOL; // This constant returns the current namespace.
+//echo __COMPILER_HALT_OFFSET__ . PHP_EOL; // It is used in conjunction with the __halt_compiler() directive to determine the offset for the __HALT_COMPILER() marker within the script.
+
+//----------------------------------------------------//
