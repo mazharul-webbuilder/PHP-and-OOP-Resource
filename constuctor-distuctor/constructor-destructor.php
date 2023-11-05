9c7062e5f0b9b625518f5bedb35c90c46ad8c74e
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-11-05 21:51:14 +0600

    constructor feature upate php 8.1+

--- constuctor-distuctor/constructor-destructor.php
+++ constuctor-distuctor/constructor-destructor.php
@@ -1,1 +1,41 @@
 <?php
+
+//-------------------------------------------------------------------------------------------------------//
+
+/**
+ * Below constructor feature will work only in PHP 8.1+
+*/
+
+//-------------------------------------------------------------------------------------------------------//
+
+
+// Before 8.1
+class ConstructorBefore
+{
+    public string $name;
+    public int $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------//
+
+// After 8.1+
+class ConstructorAfter
+{
+    /**
+     * Here in parameter $name and $age will act as class property of ConstructorAfter class
+     *
+     * remember you must have to pass access modifier Pub, Pri, Pro, Otherwise constructor will
+     * act as normal parameter
+    */
+    public function __construct(public string $name, public int $age)
+    {
+
+    }
+}
+//-------------------------------------------------------------------------------------------------------//
