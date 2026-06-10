a591eada771fb3f9ce89b1f006e84b24d9369baf
Mazharul Islam <58305538+mazharul-webbuilder@users.noreply.github.com>
2026-06-10 17:48:16 +0600

    ispirme

---
+++ interview_problem_solving/is-prime.php
@@ -0,0 +1,19 @@
+<?php
+
+function isPrime($n)
+{
+      if($n < 2){
+        return false;
+    }
+
+    for($i=2; $i<$n; $i++){
+        if($n % $i === 0){
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+echo isPrime(9);
